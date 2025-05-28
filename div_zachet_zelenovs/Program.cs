264dff51373b9b30b9d9854d145bbea384b34294
crupers <157301409+crupers@users.noreply.github.com>
2025-05-28 09:59:28 +0500

    Add files via upload

---
+++ div_zachet_zelenovs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Letters = { 'A', 'B', 'C' };
+            int[] Numbers = { 1, 2, 3 };
+            string[] Colors = { "зеленый", "ораньжевый" };
+
+            var cartesain = Letters.SelectMany(l => Numbers.SelectMany(n => Colors.Select(c => new { Letter = 1, Number = n, Color = c })));
+            Console.WriteLine("декартовы произведения");
+            foreach(var item in cartesain)
+            {
+                Console.WriteLine($"буква = {item.Letter}  число= {item.Number}  цвет = {item.Color}");
+            }
+
+            Console.WriteLine("\n введите признак для группировки(Letter/Number/Color)");
+            string group = Console.ReadLine();
+            var grouped = cartesain.GroupBy(item => group == "Letter" ? item.Letter.ToString() : group == "Number" ? item.Number.ToString() : item.Color);
+        foreach(var groups in grouped)
+            {
+                Console.WriteLine($"\n группа по {group} = {groups.Key}:");
+                foreach(var item in groups)
+                {
+                    Console.WriteLine($"буква : {item.Letter} , число = {item.Number} , цвет = {item.Color}");
+                }
+
+            }
+        }
+    }
+}
